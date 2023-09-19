e949498f5b56a1bdbfddd855949872d29828260b
Redsilver2 <Marvintanonkou@gmail.com>
2023-09-19 09:07:37 -0400

    Added Code

---
+++ MonProjet2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonProjet2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrer votre nom: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("\n\t\t\tBonjour " + username + ",\n\t\t\tBienvenu à MonProjet2");
+            Console.ReadKey();
+        }
+    }
+}
